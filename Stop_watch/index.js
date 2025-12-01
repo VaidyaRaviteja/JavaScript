29d4134f12bf8480faef7d495ae2708d706c48d4
Raviteja <vydyatej@mail.com>
2025-12-01 06:11:28 +0530

    stop_watc initial commit

---
+++ Stop_watch/index.js
@@ -0,0 +1,43 @@
+let secondsElapsed = 0;
+let interval = null;
+const time = document.getElementById("time")
+
+function padStart(value){
+    return String(value).padStart(2,"0")
+}
+
+function setTime() {
+    const minutes = Math.floor(secondsElapsed / 60)
+
+    const seconds = secondsElapsed % 60
+    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`
+}
+
+function timer(){
+    secondsElapsed++;
+    setTime()
+}
+function startClock(){
+    if (interval) stopClock()
+    interval = setInterval(timer, 1000)
+}
+
+function stopClock(){
+    clearInterval(interval)
+}
+
+
+function resetClock() {
+    stopClock()
+    secondsElapsed = 0;
+    setTime()
+}
+
+// String.padStart(targetLength, padstring)
+
+// "0".padStart(2,"0")
+
+// function padStart()
+
+// s ="0".padStart(2,"0")
+// console.log(s)
